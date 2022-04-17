f000eadc5f5ece96d18876a0d316ee1c5cb0cdc8
ethanblake4 <ethanblake@gmail.com>
2022-04-16 22:29:02 -0600

    Add Duration

---
+++ lib/src/eval/runtime/stdlib/core/duration.dart
@@ -0,0 +1,128 @@
+import 'package:dart_eval/dart_eval_bridge.dart';
+import 'package:dart_eval/src/eval/bridge/declaration/class.dart';
+import 'package:dart_eval/src/eval/bridge/declaration/function.dart';
+import 'package:dart_eval/src/eval/bridge/declaration/type.dart';
+import 'package:dart_eval/src/eval/compiler/compiler.dart';
+import 'package:dart_eval/src/eval/runtime/runtime.dart';
+import 'package:dart_eval/src/eval/shared/types.dart';
+
+class $Duration implements Duration, $Instance {
+  static void configureForCompile(Compiler compiler) {
+    compiler.defineBridgeClass($declaration);
+  }
+
+  static void configureForRuntime(Runtime runtime) {
+    runtime.registerBridgeFunc('dart:core', 'Duration.', $Function((_runtime, target, args) {
+      return $Duration.wrap(Duration(
+          days: args[0]?.$value ?? 0,
+          hours: args[1]?.$value ?? 0,
+          minutes: args[2]?.$value ?? 0,
+          seconds: args[3]?.$value ?? 0,
+          milliseconds: args[4]?.$value ?? 0,
+          microseconds: args[5]?.$value ?? 0));
+    }));
+  }
+
+  static const $type = BridgeTypeReference.unresolved(BridgeUnresolvedTypeReference('dart:core', 'Duration'), []);
+
+  static const $declaration = BridgeClassDeclaration($type, isAbstract: true, constructors: {
+    '': BridgeConstructorDeclaration(
+        false,
+        BridgeFunctionDescriptor(BridgeTypeAnnotation($type, false), {}, [], {
+          'days': BridgeParameter(
+              'days', BridgeTypeAnnotation(BridgeTypeReference.type(RuntimeTypes.intType, []), true), true),
+          'hours': BridgeParameter(
+              'hours', BridgeTypeAnnotation(BridgeTypeReference.type(RuntimeTypes.intType, []), true), true),
+          'minutes': BridgeParameter(
+              'minutes', BridgeTypeAnnotation(BridgeTypeReference.type(RuntimeTypes.intType, []), true), true),
+          'seconds': BridgeParameter(
+              'seconds', BridgeTypeAnnotation(BridgeTypeReference.type(RuntimeTypes.intType, []), true), true),
+          'milliseconds': BridgeParameter(
+              'milliseconds', BridgeTypeAnnotation(BridgeTypeReference.type(RuntimeTypes.intType, []), true), true),
+          'microseconds': BridgeParameter(
+              'microseconds', BridgeTypeAnnotation(BridgeTypeReference.type(RuntimeTypes.intType, []), true), true),
+        }))
+  }, methods: {
+    '*': BridgeMethodDeclaration(
+        false,
+        BridgeFunctionDescriptor(BridgeTypeAnnotation($type, false), {}, [
+          BridgeParameter(
+              'factor', BridgeTypeAnnotation(BridgeTypeReference.type(RuntimeTypes.numType, []), false), false)
+        ], {}))
+  }, getters: {}, setters: {}, fields: {});
+
+  $Duration.wrap(this.$value);
+
+  @override
+  final Duration $value;
+
+  @override
+  Duration get $reified => $value;
+
+  @override
+  $Value? $getProperty(Runtime runtime, String identifier) {
+    throw UnimplementedError();
+  }
+
+  @override
+  void $setProperty(Runtime runtime, String identifier, $Value value) {
+    throw UnimplementedError();
+  }
+
+  @override
+  int get $runtimeType => RuntimeTypes.durationType;
+
+  @override
+  Duration operator *(num factor) => $value * factor;
+
+  @override
+  Duration operator +(Duration other) => $value + other;
+
+  @override
+  Duration operator -() => -$value;
+
+  @override
+  Duration operator -(Duration other) => $value - other;
+
+  @override
+  bool operator <(Duration other) => $value < other;
+
+  @override
+  bool operator <=(Duration other) => $value <= other;
+
+  @override
+  bool operator >(Duration other) => $value > other;
+
+  @override
+  bool operator >=(Duration other) => $value >= other;
+
+  @override
+  Duration abs() => $value.abs();
+
+  @override
+  int compareTo(Duration other) => $value.compareTo(other);
+
+  @override
+  int get inDays => $value.inDays;
+
+  @override
+  int get inHours => $value.inHours;
+
+  @override
+  int get inMicroseconds => $value.inMicroseconds;
+
+  @override
+  int get inMilliseconds => $value.inMilliseconds;
+
+  @override
+  int get inMinutes => $value.inMinutes;
+
+  @override
+  int get inSeconds => $value.inSeconds;
+
+  @override
+  bool get isNegative => $value.isNegative;
+
+  @override
+  Duration operator ~/(int quotient) => $value ~/ quotient;
+}
