e81b957e8be966e6cde94bb9b831f24e5f1431ae
wanpat anantapan <oneplus1000@gmail.com>
2023-04-18 18:48:26 +0700

    add test for datetime

--- test/datetime_test.dart
+++ test/datetime_test.dart
@@ -2,16 +2,52 @@ import 'package:dart_eval/dart_eval.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  test('not equal for strng', () async {
+  test('datetime parse', () async {
     //DateTime x = DateTime.now();
     //x.month
     final source = '''
       bool fn(){ 
-        String x = "a";
-        if(x != "x"){
-          return true;
+        var a = DateTime.parse('2011-10-20');
+        var b = DateTime.parse('2011-10-20');
+        var c = DateTime.parse('2011-10-21');
+        if(a != b){
+          return false;
         }
-        return false;
+        if(a == c){
+          return false;
+        }
+        return true;
+      }
+      ''';
+    final compiler = Compiler();
+    final program = compiler.compile({
+      'my_package': {
+        'code.dart': source,
+      }
+    });
+    var runtime = Runtime.ofProgram(program);
+    runtime.setup();
+    var result = runtime.executeLib(
+      "package:my_package/code.dart",
+      "fn",
+    );
+    assert(result);
+  });
+
+  test('datetime year month day', () async {
+    final source = '''
+      bool fn(){ 
+        var a = DateTime.parse('2011-10-20');
+        if(a.day != 20){
+          return false;
+        }
+        if(a.month != 10 ){
+          return false;
+        }
+        if(a.year != 2011){
+          return false;
+        }
+        return true;
       }
       ''';
     final compiler = Compiler();
