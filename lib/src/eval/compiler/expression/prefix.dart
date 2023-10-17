a6dded3ea83df1887ba4f7f4cabb1cdc2bc9647b
Marcin Wróblewski <nicram606@gmail.com>
2023-10-17 20:51:44 +0200

    Avoid multiple expression compilations, fix tests, fix prefix behavior

--- lib/src/eval/compiler/expression/prefix.dart
+++ lib/src/eval/compiler/expression/prefix.dart
@@ -3,6 +3,7 @@ import 'package:analyzer/dart/ast/token.dart';
 import 'package:dart_eval/dart_eval_bridge.dart';
 import 'package:dart_eval/src/eval/compiler/builtins.dart';
 import 'package:dart_eval/src/eval/compiler/context.dart';
+import 'package:dart_eval/src/eval/compiler/reference.dart';
 import 'package:dart_eval/src/eval/compiler/type.dart';
 import 'package:dart_eval/src/eval/compiler/variable.dart';
 import 'package:dart_eval/src/eval/runtime/runtime.dart';
@@ -19,10 +20,17 @@ const _opMap = {
 
 /// Compile a [PrefixExpression] to EVC bytecode
 Variable compilePrefixExpression(CompilerContext ctx, PrefixExpression e) {
-  var V = compileExpression(e.operand, ctx);
-
   final method = _opMap[e.operator.type] ??
       (throw CompileError('Unknown unary operator ${e.operator.type}'));
+
+  if ([TokenType.PLUS_PLUS, TokenType.MINUS_MINUS].contains(e.operator.type)) {
+    final V = compileExpressionAsReference(e.operand, ctx);
+    final L = V.getValue(ctx);
+    final type = V.resolveType(ctx);
+    return _handleDoubleOperands(e, ctx, V, type, L);
+  }
+
+  final V = compileExpression(e.operand, ctx);
 
   if (method == '-' &&
       V.type != CoreTypes.int.ref(ctx) &&
@@ -38,10 +46,6 @@ Variable compilePrefixExpression(CompilerContext ctx, PrefixExpression e) {
     return V.invoke(ctx, method, []).result;
   }
 
-  if ([TokenType.PLUS_PLUS, TokenType.MINUS_MINUS].contains(e.operator.type)) {
-    return _handleDoubleOperands(e, ctx);
-  }
-
   return _zeroForType(V.type, ctx).push(ctx).invoke(ctx, method, [V]).result;
 }
 
@@ -55,26 +59,30 @@ BuiltinValue _oneForType(TypeRef type, CompilerContext ctx) =>
         ? BuiltinValue(intval: 1)
         : BuiltinValue(doubleval: 1.0);
 
-Variable _handleDoubleOperands(PrefixExpression e, CompilerContext ctx) {
-  var V = compileExpression(e.operand, ctx);
-  final ref = compileExpressionAsReference(e.operand, ctx);
-  final L = ref.getValue(ctx);
+Variable _handleDoubleOperands(
+  PrefixExpression e,
+  CompilerContext ctx,
+  Reference V,
+  TypeRef type,
+  Variable L,
+) {
   var out = L;
 
   if (L.name != null) {
     out = Variable.alloc(ctx, L.type);
     ctx.pushOp(PushNull.make(), PushNull.LEN);
-    ctx.pushOp(CopyValue.make(out.scopeFrameOffset, L.scopeFrameOffset),
-        CopyValue.LEN);
+    ctx.pushOp(
+      CopyValue.make(out.scopeFrameOffset, L.scopeFrameOffset),
+      CopyValue.LEN,
+    );
   }
 
-  ref.setValue(
+  final result = out.invoke(
     ctx,
-    L.invoke(
-      ctx,
-      _opMap[e.operator.type]!,
-      [_oneForType(V.type, ctx).push(ctx)],
-    ).result,
-  );
-  return out;
+    _opMap[e.operator.type]!,
+    [_oneForType(type, ctx).push(ctx)],
+  ).result;
+
+  V.setValue(ctx, result);
+  return result;
 }
