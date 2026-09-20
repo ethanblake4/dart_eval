37fab521b6fab2c904defedfba82c537bc34d909
khoadng <nguyenduckhoa1996@gmail.com>
2026-09-20 09:50:56 +0000

    fix: cannot box Object type in branch resolution
    Co-Authored-By: Ethan Elshyeb <ethanblake@gmail.com>

--- lib/src/eval/compiler/variable.dart
+++ lib/src/eval/compiler/variable.dart
@@ -164,7 +164,8 @@ class Variable {
 
     ctx as CompilerContext;
 
-    if (type == CoreTypes.dynamic.ref(ctx)) {
+    if (type == CoreTypes.dynamic.ref(ctx) ||
+        type == CoreTypes.object.ref(ctx)) {
       return copyWith(
         type: type.copyWith(boxed: true),
         representation: MachineRepresentation.object,
